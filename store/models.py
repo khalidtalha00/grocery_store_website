43b25fc758761d7de11b3a84d0bfb33291830da9
Talha Khalid <117529116+khalidtalha00@users.noreply.github.com>
2024-09-16 22:38:26 +0530

    made changes to models,views,templates,added functionalities to cart etc

--- store/models.py
+++ store/models.py
@@ -21,16 +21,31 @@ class Product(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
     date_ordered= models.DateTimeField(auto_now_add=True,blank=False)
+    complete = models.BooleanField(default=False,null=True,blank=False)
     transaction_id = models.CharField(max_length=200,null=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total() for item in orderitems])
+        return total
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
     order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
     quantity=models.IntegerField(default=0,null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)    
+
+    def get_total(self):
+        total = self.product.price *self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
